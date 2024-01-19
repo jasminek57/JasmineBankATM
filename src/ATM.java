34dd74f81640877740af5d92fcdf042c8dee9cc3
BT_1N3_07 <sample@bths.edu>
2024-01-19 08:59:13 -0500

    60% complete

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,16 @@
 public class ATM {
     // sll stm functions will be here
     public ATM() {}
+    public void withdraw(double account, double amount){
+        account -= amount;
+    }
+
+    public void deposit(double account, double amount){
+        account += amount;
+    }
+
+    public void transfer(Account transferring, Account getTransfer, double transfer){
+        transferring.amount -= transfer;
+        getTransfer.amount += transfer;
+    }
 }
--- src/Account.java
+++ src/Account.java
@@ -1,9 +1,17 @@
 public class Account {
     private int amount;
-
+    public ATM atm;
+    public Account checkings;
     public Account(){
         this.amount = 0;
+        atm = new ATM();
     }
 
+    public double getAmount(){
+        return amount;
+    }
+     public void updateAmount(String method){
+        amount.method();
+     }
 
 }
